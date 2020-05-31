41fcf23b4f484215cde23dd60c23ae9b70d4538b
Raghav <55020567+Raghav-byte@users.noreply.github.com>
2020-05-31 22:50:19 +0530

    Create E0006.cpp

---
+++ HackerRank/C/Easy/E0006.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+int main()
+{ 
+    int n;
+scanf("%d",&n);
+
+char *a[9]={"one","two","three","four","five","six","seven","eight","nine"};
+//'*' for long string in the array
+
+if(n>9){ 
+    printf("Greater than 9");
+}
+else
+{
+   puts(a[n-1]);
+}
+}
