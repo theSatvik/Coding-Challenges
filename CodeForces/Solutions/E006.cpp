7db10c212a774b0ff3d9994f17a22ba1ecfe38ae
Raghav <55020567+Raghav-byte@users.noreply.github.com>
2020-06-13 02:22:38 +0530

    Create E006.cpp

---
+++ CodeForces/Solutions/E006.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int n,k,arr[100],count=0;
+    cin>>n>>k;
+	for(int i=0;i<n;++i)
+	{
+		cin>>arr[i];
+	}
+	for(i=0;i<n;++i)
+{
+		if(arr[i]>=arr[k-1]  a[i]>0)
+	count++;
+}
+	cout<<count<<endl;
+	return 0;
+}
