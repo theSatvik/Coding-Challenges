5ec83355ba4e99c91be752eba1b42b8e31abb2a1
Raghav <55020567+Raghav-byte@users.noreply.github.com>
2020-06-16 22:32:58 +0530

    Update E008.cpp

--- CodeForces/Solutions/E008.cpp
+++ CodeForces/Solutions/E008.cpp
@@ -1,1 +1,26 @@
-s
+#include<bits/stdc++.h>
+using namespace std;
+
+void func(string q)
+{
+    string ans;
+    int s=q.size();
+    cout<<q[0];
+    for(int i=1 ; i<s; i+=2)
+    cout<<q[i];
+	cout<<endl;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+    	string a;
+    	cin>>a;
+    	func(a);
+	}
+	
+    return 0;
+}
