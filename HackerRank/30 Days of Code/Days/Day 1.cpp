7d8f2d727f1ca0113a8add14246431b900eb91eb
Raghav <55020567+Raghav-byte@users.noreply.github.com>
2020-06-19 20:59:42 +0530

    Create Day 1.cpp

---
+++ HackerRank/30 Days of Code/Days/Day 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+int i1;
+double d1;
+string s1;
+
+cin >> i1;
+cin >> d1;
+getchar();//cin.ignore();
+getline(cin,s1);
+
+s.append(s1);
+cout << i+i1<<fixed<<setprecision(1) <<endl<<d+d1 <<endl <<s ;
+    return 0;
