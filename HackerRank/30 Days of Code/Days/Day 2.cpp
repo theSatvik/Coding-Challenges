d9c6a749d7f2fc13fa9bf46b4b49c5c4ee928771
Raghav <55020567+Raghav-byte@users.noreply.github.com>
2020-06-19 21:00:39 +0530

    Create Day 2.cpp

---
+++ HackerRank/30 Days of Code/Days/Day 2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+  
+cout<<round(meal_cost*(1+0.01*(tip_percent +tax_percent)));
+}
+int main()
+{
+    double meal_cost;
+    cin >> meal_cost;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int tip_percent;
+    cin >> tip_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int tax_percent;
+    cin >> tax_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    solve(meal_cost, tip_percent, tax_percent);
+    return 0;
+}
