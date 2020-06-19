71390c3eb802c41246a88ff4ab6c3be9943dbc05
Raghav <55020567+Raghav-byte@users.noreply.github.com>
2020-06-19 21:01:27 +0530

    Create Day 3.cpp

---
+++ HackerRank/30 Days of Code/Days/Day 3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    if( N%2 == 1 || (N > 5 && N < 21))
+
+    printf("Weird");
+
+else
+
+    printf("Not Weird");
+
+return 0;
+}
