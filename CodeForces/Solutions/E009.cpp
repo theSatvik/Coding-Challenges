1faddf09eba2964672bceb640b0f34bac05b476f
Satvik Shrivas <44926681+theSatvik@users.noreply.github.com>
2020-10-03 01:19:19 +0530

    Update E009.cpp

--- CodeForces/Solutions/E009.cpp
+++ CodeForces/Solutions/E009.cpp
@@ -3,19 +3,27 @@ using namespace std;
  
 int main()
 {
-      int num,moves;
-    for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<5;j++)
-        {
-            cin>>num;
-            if(num==1)
-            {
-              moves=abs(2-i)+abs(2-j);
-            }
-        }
-    }
-    cout<<moves<<endl;
- 
+      int CountVal=0;  // Number of values before 1 has occurred while traversing the matrix in Row - Major Order 
+      int Value;       // Represents individual values 
+	cin>>Value;
+	while(Value!=1) {    // Loop until 1 doesnot occur
+		cin>>Value;
+		CountVal++;   
+	}
+	cout<<abs(CountVal%5-2) + abs(CountVal/5-2);   
+	// abs(CountVal%5-2) it eliminates the rows before the row which contains 1 and calculates the difference from the center element of that row 
+	//     0 0 0 0 0		0 0 0 0 0		
+ 	//     0 0 0 1 0		0 0 1 0 0    
+	//     0 0 0 0 0      --->	0 0 0 0 0
+	//     0 0 0 0 0		0 0 0 0 0
+	//     0 0 0 0 0		0 0 0 0 0
+	
+	// and  abs(CountVal/5-2) determines the difference or the steps needed to align 1 at the center of matrix by finding vertical differnence    
+	//     0 0 0 0 0		0 0 0 0 0		
+ 	//     0 0 1 0 0		0 0 0 0 0    
+	//     0 0 0 0 0      --->	0 0 1 0 0
+	//     0 0 0 0 0		0 0 0 0 0
+	//     0 0 0 0 0		0 0 0 0 0
+	
 	return 0;
 }
